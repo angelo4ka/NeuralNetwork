515a117959d7a31c465e12f15fa4a314d5664592
angelo4ka <karina3125@yandex.ru>
2022-03-01 10:59:11 +0300

    Add project files.

---
+++ NeuralNetwork/Vector.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    /// <summary>
+    /// Вектор (входные, выходные)
+    /// </summary>
+    class Vector
+    {
+        public double[] v; // Значения вектора
+        public int n; // Длина вектора
+
+        /// <summary>
+        /// Создание вектора из количества элементов (длины)
+        /// </summary>
+        /// <param name="n">Длина вектора</param>
+        public Vector(int n)
+        {
+            this.n = n; // Копируем длину
+            v = new double[n]; // Создаём массив
+        }
+
+        /// <summary>
+        /// Создание вектора из перечисления вещественных чисел (значений)
+        /// </summary>
+        /// <param name="values">Массив вещественных чисел</param>
+        public Vector(params double[] values)
+        {
+            n = values.Length; // Находим длину 
+            v = new double[n]; // Создаём массив
+
+            // Заполняем массив
+            for (int i = 0; i < n; i++)
+                v[i] = values[i];
+        }
+
+        /// <summary>
+        /// Обращение к значению вектора по индексу
+        /// </summary>
+        /// <param name="i">Индекс</param>
+        /// <returns>Значение вектора</returns>
+        public double this[int i]
+        {
+            get { return v[i]; } // Получение значения
+            set { v[i] = value; } // Изменение значения
+        }
+    }
+}
